67c977697308a7ca17089e3d9321c9ddf0b407b3
Ian Blake <ian@ianbl8.com>
2023-08-07 13:13:26 +0100

    Updates stories for cast/crew queries

--- src/stories/movieDetails.stories.jsx
+++ src/stories/movieDetails.stories.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router";
 import MovieDetails from "../components/movieDetails";
 import MoviesContextProvider from "../contexts/moviesContext";
 import SampleMovie from "./sampleMovie";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default {
   title: "Movie Details Page/MovieDetails",
@@ -10,6 +21,9 @@ export default {
   decorators: [
     (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
     (Story) => <MoviesContextProvider>{Story()}</MoviesContextProvider>,
+    (Story) => (
+      <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
+    ),
   ],
 };
 
--- src/stories/tvShowDetails.stories.jsx
+++ src/stories/tvShowDetails.stories.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router";
 import TVShowDetails from "../components/tvShowDetails";
 import TVShowsContextProvider from "../contexts/tvShowsContext";
 import SampleTVShow from "./sampleTVShow";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default {
   title: "TV Show Details Page/TVShowDetails",
@@ -10,6 +21,9 @@ export default {
   decorators: [
     (Story) => <MemoryRouter initialEntries={["/"]}>{Story()}</MemoryRouter>,
     (Story) => <TVShowsContextProvider>{Story()}</TVShowsContextProvider>,
+    (Story) => (
+      <QueryClientProvider client={queryClient}>{Story()}</QueryClientProvider>
+    ),
   ],
 };
 
